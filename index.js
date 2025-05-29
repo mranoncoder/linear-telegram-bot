7edac2b0dcf6506c4c1d6a08c3d5cea19fcbaf4a
mranoncoder <inbox@abolfazl.de>
2025-05-29 13:27:24 +0200

    added express

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import 'dotenv/config'
 import { Telegraf } from 'telegraf'
+import express from 'express'
 import addIssue from './commands/addIssue.js'
 import listTasks from './commands/listTasks.js'
 import markDone from './commands/markDone.js'
 import setInProgress from './commands/setInProgress.js'
 import deleteTask from './commands/deleteTask.js'
 
+const app = express()
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN)
 
 bot.command('add', addIssue)
@@ -14,5 +16,10 @@ bot.command('done', markDone)
 bot.command('progress', setInProgress)
 bot.command('delete', deleteTask)
 bot.command('ping', async (ctx) => ctx.reply(ctx.chat.id))
-bot.launch()
-console.log('🤖 Bot is running...')
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`🤖 Server is running on port ${port}...`)
+  bot.launch()
+  console.log('🤖 Bot is running...')
+})
